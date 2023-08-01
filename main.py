2ff24fa013c030df08771b62895bcc026c903fe1
Your Name <you@example.com>
2023-08-01 14:20:40 +0530

    Code to move horizontally using keyboard input

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Jumpy")
 
+# set frame rate
+clock = pygame.time.Clock()
+FPS = 60
+
 # define colours
 WHITE = (255, 255, 255)
 
@@ -28,9 +32,35 @@ class Player:
         self.height = 40
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = (x, y)
+        self.flip = False
+
+    def move(self):
+        # reset variables
+        dx = 0
+        dy = 0
+
+        # process key presses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a] or key[pygame.K_LEFT]:
+            dx = -10
+            self.flip = True
+        if key[pygame.K_d] or key[pygame.K_RIGHT]:
+            dx = 10
+            self.flip = False
+
+        # ensure player doesn't go off the edge of the screen
+        if self.rect.left + dx < 0:
+            dx = - self.rect.left
+        if self.rect.right + dx > SCREEN_WIDTH:
+            dx = SCREEN_WIDTH - self.rect.right
+
+        # update rectangle position
+        self.rect.x += dx
+        self.rect.y += dy
 
     def draw(self):
-        screen.blit(self.image, (self.rect.x - 12, self.rect.y - 5))
+
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), (self.rect.x - 12, self.rect.y - 5))
         pygame.draw.rect(screen, WHITE, self.rect, 2)
 
 
@@ -39,6 +69,10 @@ jumpy = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
 # game loop
 run = True
 while run:
+
+    clock.tick(FPS)
+
+    jumpy.move()
 
     # draw background
     screen.blit(bg_image, (0, 0))
