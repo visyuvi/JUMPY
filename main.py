eb9218799dc62eb18e9038979d73b3e4d1136aac
Your Name <you@example.com>
2023-08-01 14:31:22 +0530

    Added gravity and jumping

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ pygame.display.set_caption("Jumpy")
 clock = pygame.time.Clock()
 FPS = 60
 
+# game variables
+GRAVITY = 1
+
 # define colours
 WHITE = (255, 255, 255)
 
@@ -32,6 +35,7 @@ class Player:
         self.height = 40
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = (x, y)
+        self.vel_y = 0
         self.flip = False
 
     def move(self):
@@ -48,11 +52,21 @@ class Player:
             dx = 10
             self.flip = False
 
+        # gravity
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+
         # ensure player doesn't go off the edge of the screen
         if self.rect.left + dx < 0:
             dx = - self.rect.left
         if self.rect.right + dx > SCREEN_WIDTH:
             dx = SCREEN_WIDTH - self.rect.right
+
+        # check collision with ground
+        if self.rect.bottom + dy > SCREEN_HEIGHT:
+            dy = 0
+            # bounce off the ground
+            self.vel_y = -20
 
         # update rectangle position
         self.rect.x += dx
